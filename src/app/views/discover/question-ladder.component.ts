57754cd26849921930f84eb47b3bda1d6ecc19a9
lhernandezc <lhernandezc@actinver.com.mx>
2019-11-23 09:21:46 -0600

    Discover tools

--- src/app/views/discover/question-ladder.component.ts
+++ src/app/views/discover/question-ladder.component.ts
@@ -1,8 +1,52 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CommentModel} from '../../model/comment.model';
+import {TeamMemberModel} from '../../model/teamMember.model';
 
 @Component({
   templateUrl: './question-ladder.component.html'
 })
-export class QuestionLadderComponent {
+export class QuestionLadderComponent implements OnInit {
 
+  public comments: CommentModel[] = [];
+  public commentToAdd: CommentModel = new CommentModel();
+  public showComments: boolean = false;
+
+  ngOnInit(): void {
+    this.initComments();
+  }
+
+  toggleComments() {
+    this.showComments = !this.showComments;
+  }
+
+  addComment() {
+    const team2 = new TeamMemberModel();
+    team2.name = 'Luis Daniel H.';
+    this.commentToAdd.date = new Date();
+    this.commentToAdd.user = team2;
+    this.comments.push(Object.assign({}, this.commentToAdd));
+    this.commentToAdd = new CommentModel();
+  }
+
+  initComments() {
+    const team1 = new TeamMemberModel();
+    team1.name = 'Marcelo T.';
+    const team2 = new TeamMemberModel();
+    team2.name = 'Luis Daniel H.';
+    const c1 = new CommentModel();
+    c1.user = team1;
+    c1.date = new Date();
+    c1.body = 'Me parece que el avance está bien. Adelante con la siguiente actividad';
+    const c2 = new CommentModel();
+    c2.user = team1;
+    c2.date = new Date();
+    c2.body = 'Me parece que el avance está bien. Adelante con la siguiente actividad';
+    const c3 = new CommentModel();
+    c3.user = team1;
+    c3.date = new Date();
+    c3.body = 'Me parece que el avance está bien. Adelante con la siguiente actividad';
+    this.comments.push(c1);
+    this.comments.push(c2);
+    this.comments.push(c3);
+  }
 }
